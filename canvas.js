47fa8a8f2cdf6c9c61ead7b11bfc92105131aa67
octobrekids <chnrd.k@gmail.com>
2021-04-05 23:51:11 +0700

    draw a line

--- canvas.js
+++ canvas.js
@@ -6,10 +6,9 @@ window.addEventListener('load', () => {
 	canvas.height = window.innerHeight;
 	canvas.width = window.innerWidth;
 
-	// need to define before use
-	context.strokeStyle = 'red';
-	context.lineWidth = 5;
-	context.strokeRect(50, 50, 200, 200);
-	context.strokeStyle = 'blue';
-	context.strokeRect(100, 100, 200, 200);
+	context.beginPath();
+	// start position
+	context.moveTo(100, 100);
+	context.lineTo(200, 100);
+	context.stroke();
 });
