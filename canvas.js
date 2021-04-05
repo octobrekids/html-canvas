523e07ec8162acdf1e0cae611f39e54e5f5ff371
octobrekids <chnrd.k@gmail.com>
2021-04-05 23:49:14 +0700

    add lineWidth

--- canvas.js
+++ canvas.js
@@ -8,6 +8,7 @@ window.addEventListener('load', () => {
 
 	// need to define before use
 	context.strokeStyle = 'red';
+	context.lineWidth = 5;
 	context.strokeRect(50, 50, 200, 200);
 	context.strokeStyle = 'blue';
 	context.strokeRect(100, 100, 200, 200);
