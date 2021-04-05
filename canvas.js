ffc267ba1c771912deb3c6a78d960b0e727ae155
octobrekids <chnrd.k@gmail.com>
2021-04-05 23:48:45 +0700

    storkeRect

---
+++ canvas.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+	const canvas = document.querySelector('#canvas');
+	const context = canvas.getContext('2d');
+
+	//Resizing
+	canvas.height = window.innerHeight;
+	canvas.width = window.innerWidth;
+
+	// need to define before use
+	context.strokeStyle = 'red';
+	context.strokeRect(50, 50, 200, 200);
+	context.strokeStyle = 'blue';
+	context.strokeRect(100, 100, 200, 200);
+});
